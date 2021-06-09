2dff1c2fed85b5b4401d19929b4641b08ba32f20
BilalAhmed500 <55891838+BilalAhmed500@users.noreply.github.com>
2021-06-09 01:23:13 -0700

    Sanjalika Water Park
    This is a static  Website of a water park in this website you are able to do a booking and more . but statically there is no backend this is a front end website

---
+++ js/app.js
@@ -0,0 +1,136 @@
+$(document).ready(function () {
+
+    $('#r1').click(function () {
+        $('.modal-title').text('Turbo Cyclone')
+        $('#modalimage').attr('src', './images/turbo.jpg.png')
+        $('#modal-para').text('This, most popular, bowl slide is the extremely exciting, thrilling and attractive water slide in sanjalika Family Water Park. It allows two riders at a time drop into a big round Bowl with fast speed and then circle the outer edges of the Bowl before exiting out, through terminal flume. Definitely riders of all ages will experience an unforgettable sliding experience with loud screams along whole course. The Turbo Cyclone is a very popular slide in sanjalika Family Water Park. Riders take on a raft and grasp handles on both sides and slide down with, along the way, with its own gravity under water lubrication. After a period of spiral section, steep fall and buffer section, riders will enter the “Super Bowl“ by the spiral tangents as if they are they are sucked into the vortex. Raft will slide around the bowl under water lubrication, like flowing into the vortex. It means that riders taking a raft will come down from and go into buffer section bottom of the Bowl and then slow down on the splash pool, completing a thrilling journey. It allows two riders at a time with strong interactivity, has doughty appearance, making it an attractive slide. It makes the riders feel like being sucked into the tube and splash pool. It is made with top quality material, ensuring safe, stable and a memorable sliding experience.')
+    })
+    $('#r2').click(function () {
+        $('.modal-title').text('Pendulum')
+        $('#modalimage').attr('src', './images/Pendulam.png')
+        $('#modal-para').text('A marvel in the water park slides precisely designed and superbly finished , a scary, yet, fun-filled water slide. It is a unique water slide where riders move up and down like a Pendulum in a wide U Shaped slide before coming to a gradual halt. A slide constructed with safety and quality to meet and exceed your expectations. Our goal is to provide you with entertainment of quality which you rightly deserve. Smooth surface finish added with water lubrication makes it a thrill , a memorable adventure. Riders take on a raft and grasp handles on both sides firmly, then slide down with along the way with its own gravity under water lubrication. Due to high speed and momentum, the riders keep swinging up and down like a Pendulum. A truly thrilling and fun- filled slide equally liked by all age groups. It is made with the finest available material for extreme safety and entertainment.')
+
+    })
+    $('#r3').click(function () {
+        $('.modal-title').text('BOOMRENGO')
+        $('#modalimage').attr('src', './images/Boomrengo.png')
+        $('#modal-para').text('The riders ride on an appropriate raft, slides into the fume from a high platform, through spiral drainage; dive like a space craft to the bottom with great inertia, rush to the opposite slope, to reach the highest point at once, let the riders experience the feeling of sky walk. Even when the riders, yet, is not at rest, they will fall quickly by their own weight and momentum, entering into the recycling fume and pass the semi circular, drop into the splash pond finally. It is especially suitable for adventurous challenges. Definitely it makes riders eyes closed, their breath is at hold and they scream loudly with pleasure.')
+
+    })
+    $('#r4').click(function () {
+        $('.modal-title').text('Big Monster')
+        $('#modalimage').attr('src', './images/Big-Monstar.png')
+        $('#modal-para').text('Big Monster is one of the prime attractions at the park. Big monster is one of its kind, slides around the Pakistan. An open slide, which offers a unique and unforgettable ride experience. You will discover something new and exciting at every corner. Friends, Spouses and Parents can experience the joy of sliding altogether and laughter will be heard all around the park. A fabulous art craft, for those who want to thrill around for the sake of fun and entertainment. A soft and silky surface finish, to make the ride very smooth and memorable')
+    })
+    $('#r5').click(function () {
+        $('.modal-title').text('Rainbow')
+        $('#modalimage').attr('src', './images/RainBow.png')
+        $('#modal-para').text('This side by side multi-lane multilane racing water slide is the original mat-racer. It takes riders on a fun head-head battle, accelerating the riders towards the Finish Line. The Rainbow slide is the proven must-have ride for every water park. As a must in Water Park, the Rainbow slide is athletic sports oriented slide, special attraction for young people. The skidding players fly the smooth slide at a high speed and directly dive at an accelerated pace into pool, giving them speedy kick of head down gliding. It is remarkable sort of contest, the source of super appeal and popularity, allows visitors to enjoy neck-to-neck, shoulder-to-shoulder diving fun.')
+    })
+    $('#r6').click(function () {
+        $('.modal-title').text('Free Fall')
+        $('#modalimage').attr('src', './images/FreeFall.png')
+        $('#modal-para').text('As the name implies it is a true and tantalizing free for all fall. With water gushing all over and the feel of falling down the water lane. It is a must for all visitors.        A 60 feet high free fall is not for the weak hearts. Free fall is the Pakistan’s tallest water slide. You can choose between the rapid free fall and the clumsy free fall. A unique and thrilling slide, a very high speed slide, the Tallest, The Steepest, The Fastest, the riders will feel and experience the vertical drop. The slide bends at a thought slope, to produce the most heart pounding, head rushing and water spaying experience to the riders.')
+    })
+    $('#r7').click(function () {
+        $('.modal-title').text('Wave Pool')
+        $('#modalimage').attr('src', './images/WavePool.png')
+        $('#modal-para').text('Enjoy a great day of summers at sanjalika Family Water Park`s Waves Pool. Bring the family for a full day of summer venture. Take the adventure further and body surf in the — square feet Waves Pool with over 2 million liters of water. The gigantic waves of wave’s pool offer some of the unique and memorable moments of your life. Of course the Park`s center piece is the Giant Waves Pool. Grab a tube, bob and weaves in the surf as undulating waves give you an ocean like experience. While you are enjoying at the aquatic facilities at the waves pool, you can feel safe and secure, knowing that you are being closely monitored by experienced Life Guards. “Safety first” is the mantra of our staff, who take their job seriously and hold the safety of you and your family in the highest regard. Swim and enjoy with care, but do so knowing that our team of Life Guards is ready to assist you, should the need arise. Enjoy your day at The Waves Pool.')
+    })
+    $('#r8').click(function () {
+        $('.modal-title').text('Cyclone')
+        $('#modalimage').attr('src', './images/Cyclone.png')
+        $('#modal-para').text('It is a unique water ride with adventurous experience for all ages. It gives experience of a loop, Cyclonic spin followed by a drop into the pool. It is an introductory ride for limited thrill. It is equipped with slide guard for safety and smooth flow while sliding. It is made with Eco Friendly and Chlorine resistant materials. A highly liked and appreciated slide for its features like dimensional accuracy, precise designing, smooth surface finish and modern look. This slide will make a big splash with all age groups. Nothing creates a more universally embraced pool experience than a slide like Cyclone water slide with loads of safe entertainment.')
+    })
+    $('#r9').click(function () {
+        $('.modal-title').text('Aquatic Island + Lazy River')
+        $('#modalimage').attr('src', './images/Aquatic-Island-Lazy-River.png')
+        $('#modal-para').text('An artificial water river of adequate length, width and depth, with reasonably shallow water, which is extremely exciting and suitable for the whole family. Located centrally, Aquatic Island and Lazy River offer a wide range of exciting features and Fun activity.A pure Aqua venture pool is one of a kind. This nonstop aquatic play ground features a magnificent quarter mile Lazy River. A spectacular kids water play area. Aquatic Island is immersive, interconnected and perfect for all vacationers both young and old. sanjalika Family Water Park`s Aquatic Island and Lazy River combined, offer an extreme level of excitement and delivers an “Aqua Adventure like no other.')
+    })
+    $('#r10').click(function () {
+        $('.modal-title').text('Rim-Jhim')
+        $('#modalimage').attr('src', './images/Rim-Jhim.png')
+        $('#modal-para').text('Welcome and join us for a wet and wild fun in our Rim Jhim. Swim and splash in the pool, designed for the guests to relax and enjoy on a quite environment. Make a day of it. There is fun for everyone in sanjalika Family Water Park. Beat the Heat with moderately cold water. A perfect place, to get relief from Heat, and having Fun at the same time. Take a dip, sit back, relax and let out a sigh, because life is too short to enjoy and nice to be sitting on the shore like environments with liberty to play with water. First moments inside Rim Jhim Pool almost, always jar your senses. A rush of moist, chlorine-scented air fills your nose. And then there is continuous loop of nonstop latest popular music vying with an endless whir of water. This is Rim Jhim Pool.')
+    })
+    $('#r11').click(function () {
+        $('.modal-title').text('Family Slide')
+        $('#modalimage').attr('src', './images/FamilySlide.png')
+        $('#modal-para').text('This is an attractive and stable water slide that has been designed, developed and installed with the aim of providing fun to whole family. A wide, spacious and attractively colored slide, which enhances the photographic memories for a memorable sliding experience. It delivers operator value and a good all round water play experience. A great ride to introduce the young family members to the thrill of water sliding. It is equipped with slide guard for safety and smooth ride while sliding down, made with eco-friendly and Chlorine resistant material. An intensively liked and appreciated slide, with wide and spacious sliding body with smooth surface finish.')
+    })
+    $('#r12').click(function () {
+        $('.modal-title').text('The Jungle')
+        $('#modalimage').attr('src', './images/The-Jungle.png')
+        $('#modal-para').text('It is jungle out there, so get yourself hooked up with an amazing water slide. It is specially designed and erected for our young guests. Just cool down on a trip to sanjalika Family Water Park`s Jungle Slide, one of Pakistan`s most impressive and well equipped Family Water Park. A colorful presentation of Jungle life on the slide marks it impressive. Jungle Slide has created a stimulating experience for all age groups with an environment inspired by nature and in complete harmony with nature.')
+    })
+    $('#r13').click(function () {
+        $('.modal-title').text('Aqua-play')
+        $('#modalimage').attr('src', './images/Aqua-Play.png')
+        $('#modal-para').text('This is Special effects water attraction has been creatively deployed to give our little Visitors a joy of their lifetime where they balance themselves to reach the target by crossing 8 –Flooded Wheels.A perfect place and pool for those ladies who like to enjoy in solitude, away from male hustle n bustle, thus avoiding male presence at all. A wide and beautifully designed pool where ladies and kids (Up to 5 years of age) can relax and have nonstop fun and joy as they like and wish, without any hindrance. You may like to have a thrilling voyage on superbly crafted Titanic, slide or have rain shower. It is all up to you. Have a great day at the Ladies and Kids Pool.')
+    })
+    $('#r14').click(function () {
+        $('.modal-title').text('Mine Titanic Ship')
+        $('#modalimage').attr('src', './images/Titanic-Ship.png')
+        $('#modal-para').text('For the little ones this is yet another attraction. This colorful ship has 3 different water slides with a height of 7 feet and includes 2 Spiral slides, 2 free fall with hump and 1 Family slide.A perfect place and pool for those ladies who like to enjoy in solitude, away from male hustle n bustle, thus avoiding male presence at all. A wide and beautifully designed pool where ladies and kids (Up to 5 years of age) can relax and have nonstop fun and joy as they like and wish, without any hindrance. You may like to have a thrilling voyage on superbly crafted Titanic, slide or have rain shower. It is all up to you. Have a great day at the Ladies and Kids Pool.')
+    })
+    $('#r15').click(function () {
+        $('.modal-title').text('Cyclone pool')
+        $('#modalimage').attr('src', './images/Cyclone-Pool.png')
+        $('#modal-para').text('Welcome and join us for a wet and wild fun in our Cyclone Pool. Swim and splash in the pool, designed for the guests to relax and enjoy on a quite environment. Make a day of it. There is fun for everyone in sanjalika Family Water Park. Beat the Heat with moderately cold water. A perfect place, to get relief from Heat, and having Fun at the same time. Take a dip, sit back, relax and let out a sigh, because life is too short to enjoy and nice to be sitting on the shore like environments with liberty to play with water. First moments inside Cyclone pool almost, always jar your senses. A rush of moist, chlorine-scented air fills your nose. And then there is continuous loop of nonstop latest popular music vying with an endless whir of water. This is Cyclone Pool.')
+    })
+    var Check = true;
+    $('#ac-color').click(function () {
+
+        if (Check) {
+            $('.colchange').css('background-color', '#4934eb')
+            $('.colchange').css('color', 'white')
+            Check = false;
+        } else {
+            $('.colchange').css('background-color', 'white')
+            $('.colchange').css('color', 'black')
+            Check = true;
+        }
+
+    })
+    var two = true;
+    $('#ac-color1').click(function () {
+
+        if (two) {
+            $(".colchange1").css('background-color', '#4934eb')
+            $(".colchange1").css('color', 'white')
+            two = false;
+        } else {
+            $(".colchange1").css('background-color', 'white')
+            $(".colchange1").css('color', 'black')
+            two = true;
+        }
+
+    })
+    var three = true;
+    $('#ac-color2').click(function () {
+
+        if (three) {
+            $('.colchange2').css('background-color', '#4934eb')
+            $('.colchange2').css('color', 'white')
+            three = false;
+        } else {
+            $('.colchange2').css('background-color', 'white')
+            $('.colchange2').css('color', 'black')
+            three = true;
+        }
+
+    })
+    var four = true;
+    $('#ac-color3').click(function () {
+
+        if (four) {
+            $('.colchange3').css('background-color', '#4934eb')
+            $('.colchange3').css('color', 'white')
+            four = false;
+        } else {
+            $('.colchange3').css('background-color', 'white')
+            $('.colchange3').css('color', 'black')
+            four = true;
+        }
+
+    })
+});
